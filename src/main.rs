e96d174cbb563944800f75d12cde6f8780269341
Zachary Jones <zachary@jones.cx>
2024-08-20 13:59:39 -0400

    Transaction checkpoint: Parse payee

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 struct Transaction {
     date: String,
+    payee: String,
 }
 
-fn parse_txn(raw: String) -> Transaction {
-    return Transaction { date: raw };
+fn parse_txn(raw: String) -> Option<Transaction> {
+    let pieces = raw.split_once(" ");
+
+    match pieces {
+        Some((date, payee)) => {
+            return Some(Transaction {
+                date: date.to_string(),
+                payee: payee.to_string(),
+            });
+        }
+        None => {
+            return None;
+        }
+    }
 }
 
 #[test]
 fn test_parse_txn() {
     let date = "2012-03-10".to_string();
-    let txn = parse_txn(format!("{}", date));
+    let payee = "KFC".to_string();
+    let txn = parse_txn(format!("{} {}", date, payee));
+
+    assert!(txn.is_some());
+    let txn = txn.unwrap();
+
     assert_eq!(txn.date, "2012-03-10");
+    assert_eq!(txn.payee, "KFC");
 }
 
 fn main() {
