a1de03a23a53c519e6e80ec704b9b913ea886923
Zachary Jones <zachary@jones.cx>
2024-08-20 13:34:27 -0400

    Transaction checkpoint: Date

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct Transaction {
+    date: String,
+}
+
+fn parse_txn(raw: String) -> Transaction {
+    return Transaction { date: raw };
+}
+
+#[test]
+fn test_parse_txn() {
+    let date = "2012-03-10".to_string();
+    let txn = parse_txn(format!("{}", date));
+    assert_eq!(txn.date, "2012-03-10");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!")
 }
